30f59c3262b91c98f03025683076056afa9dd25d
Dove-white <dlsdove@gmail.com>
2024-09-27 13:28:47 +0000

    Feat: note model

---
+++ src/models/note.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+const {
+  generateRandomColor,
+} = require("../custom modules/random_background_color");
+
+// Note schema
+const { backgroundColor, textColor } = generateRandomColor();
+
+const noteSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    body: {
+      type: String,
+      required: true,
+    },
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+    background_color: {
+      type: String,
+      default: backgroundColor,
+    },
+    text_color: {
+      type: String,
+      default: textColor,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Note = mongoose.model("Note", noteSchema);
+
+module.exports = Note;
