6dfa124f78c2c86e8475b3b4f693535b09b072a1
Dove-white <dlsdove@gmail.com>
2024-09-27 13:31:10 +0000

    Add: note router to api

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const userRouter = require("./routers/user");
+const noteRouter = require("./routers/note");
 const cors = require("cors");
 require("dotenv").config();
 require("./db/db");
@@ -8,6 +9,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(userRouter);
+app.use(noteRouter);
 
 const port = process.env.PORT || 3000;
 
